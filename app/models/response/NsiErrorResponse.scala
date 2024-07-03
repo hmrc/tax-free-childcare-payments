d4a23c298bbbb11a67ab74824bf8b68038ff5692
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-07-03 12:56:46 +0100

    TFCA-261 updated NSI error responses; updated Scalastyle

--- app/models/response/NsiErrorResponse.scala
+++ app/models/response/NsiErrorResponse.scala
@@ -30,23 +30,70 @@ object NsiErrorResponse extends Status {
   private case object E0003 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
   private case object E0004 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
   private case object E0005 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
-  private case object E0006 extends NsiErrorResponse(BAD_GATEWAY, "Bad Gateway")
+  private case object E0006 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
   private case object E0007 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
-  private case object E0008 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
-  private case object E0009 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
-  private case object E0010 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
-  private case object E0020 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
-  private case object E0021 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
-  private case object E0022 extends NsiErrorResponse(BAD_GATEWAY, "Bad Gateway")
+  private case object E0008 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
+
+  private case object E0020 extends NsiErrorResponse(BAD_GATEWAY, "Bad Gateway")
+  private case object E0021 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
+  private case object E0022 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
+  private case object E0023 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
   private case object E0024 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0025 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0026 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
 
-  private case object E9000 extends NsiErrorResponse(BAD_GATEWAY, "Bad Gateway")
-  private case object E9999 extends NsiErrorResponse(BAD_GATEWAY, "Bad Gateway")
+  private case object E0401 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "The server encountered an error and couldn't process the request")
 
+  private case object E0030 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0031 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0032 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0033 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0034 extends NsiErrorResponse(SERVICE_UNAVAILABLE, "The service is currently unavailable")
+  private case object E0035 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+
+  private case object E0040 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0041 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0042 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+  private case object E0043 extends NsiErrorResponse(BAD_REQUEST, "Request data is invalid or missing")
+
+  private case object E9000 extends NsiErrorResponse(SERVICE_UNAVAILABLE, "The service is currently unavailable")
+  private case object E9999 extends NsiErrorResponse(SERVICE_UNAVAILABLE, "The service is currently unavailable")
   private case object E8000 extends NsiErrorResponse(SERVICE_UNAVAILABLE, "The service is currently unavailable")
   private case object E8001 extends NsiErrorResponse(SERVICE_UNAVAILABLE, "The service is currently unavailable")
 
-  private val values = Set(E0000, E0001, E0002, E0003, E0004, E0005, E0006, E0007, E0008, E0009, E0010, E0020, E0021, E0022, E0024, E9000, E9999, E8000, E8001)
+  private val values = Set(
+    E0000,
+    E0001,
+    E0002,
+    E0003,
+    E0004,
+    E0005,
+    E0006,
+    E0007,
+    E0008,
+    E0020,
+    E0021,
+    E0022,
+    E0023,
+    E0024,
+    E0025,
+    E0026,
+    E0401,
+    E0030,
+    E0031,
+    E0032,
+    E0033,
+    E0034,
+    E0035,
+    E0040,
+    E0041,
+    E0042,
+    E0043,
+    E9000,
+    E9999,
+    E8000,
+    E8001
+  )
 
   implicit val reads: Reads[NsiErrorResponse] = json =>
     for {
