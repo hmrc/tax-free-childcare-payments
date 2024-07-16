bd24d1d9218866e9b96df44992222859ddc40bfb
Sam.Zaroug <161817435+szaroug-hmrc-2024@users.noreply.github.com>
2024-07-16 12:39:34 +0100

    tfca-308: removed logging statements

--- it/test/controllers/TaxFreeChildcarePaymentsControllerISpec.scala
+++ it/test/controllers/TaxFreeChildcarePaymentsControllerISpec.scala
@@ -18,7 +18,6 @@ package controllers
 
 import base.{BaseISpec, JsonGenerators, NsiStubs}
 import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, stubFor}
-import play.api.libs.Files.logger
 import play.api.libs.json.{JsString, Json}
 
 import java.util.UUID
@@ -142,17 +141,13 @@ class TaxFreeChildcarePaymentsControllerISpec extends BaseISpec with NsiStubs wi
 
           stubNsiMakePayment201(expectedNsiResponseBody)
 
-          var arg = validPaymentRequestWithPayeeTypeSetToCCP.sample.get
-          logger.info("--- here")
-          logger.info(arg.toString())
-
           val res = ws
             .url(s"$baseUrl/")
             .withHttpHeaders(
               AUTHORIZATION  -> "Bearer qwertyuiop",
               CORRELATION_ID -> expectedCorrelationId.toString
             )
-            .post(arg)
+            .post(validPaymentRequestWithPayeeTypeSetToCCP.sample.get)
             .futureValue
 
           val resCorrelationId = UUID fromString res.header(CORRELATION_ID).value
