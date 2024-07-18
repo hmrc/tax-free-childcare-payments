b8350eeba2089038f02c0181722511c0435808f3
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-07-19 00:02:01 +0100

    TFCA-288 impl-ed failing AuthActionISpec

---
+++ it/test/controllers/actions/AuthActionISpec.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import base.BaseISpec
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.{okJson, stubFor}
+import models.requests.IdentifierRequest
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream.scaladsl.StreamConverters
+import play.api.libs.json.{JsDefined, JsString, Json}
+import play.api.mvc.{AnyContentAsEmpty, Result, Results}
+import play.api.test.FakeRequest
+
+import java.util.UUID
+import scala.concurrent.Future
+
+class AuthActionISpec extends BaseISpec with Results {
+  private implicit val as: ActorSystem = app.actorSystem
+
+  private val authAction = app.injector.instanceOf[AuthAction]
+
+  "method invokeBlock" should {
+    "return a 400 Response with errorCode ETFC1 and expected errorDescription" when {
+      "correlation ID is missing" in withAuthNinoRetrieval {
+        val requestSansCorrelationId = FakeRequest().withHeaders(AUTHORIZATION -> "Bearer a-totally-random-token")
+
+        val actualResult = authAction.invokeBlock(requestSansCorrelationId, successBlock[AnyContentAsEmpty.type]).futureValue
+
+        checkErrorResult(actualResult, BAD_REQUEST, "ETFC1", EXPECTED_400_ERROR_DESCRIPTION)
+      }
+
+      "correlation ID is invalid" in withAuthNinoRetrieval {
+        val requestSansCorrelationId = FakeRequest().withHeaders(
+          AUTHORIZATION -> "Bearer a-totally-random-token",
+          CORRELATION_ID -> "an-invalid-uuid"
+        )
+
+        val actualResult = authAction.invokeBlock(requestSansCorrelationId, successBlock[AnyContentAsEmpty.type]).futureValue
+
+        checkErrorResult(actualResult, BAD_REQUEST, "ETFC1", EXPECTED_400_ERROR_DESCRIPTION)
+      }
+    }
+
+    "return a 500 response with errorCode ETFC1 and expected errorDescription" when {
+      "Auth doesn't return a NI number" in {
+        stubFor(WireMock.post("/auth/authorise") willReturn okJson("{}"))
+
+        val requestSansCorrelationId = FakeRequest().withHeaders(
+          AUTHORIZATION -> "Bearer a-totally-random-token",
+          CORRELATION_ID -> UUID.randomUUID().toString
+        )
+
+        val actualResult = authAction.invokeBlock(requestSansCorrelationId, successBlock[AnyContentAsEmpty.type]).futureValue
+
+        checkErrorResult(actualResult, INTERNAL_SERVER_ERROR, "ETFC2", EXPECTED_500_ERROR_DESCRIPTION)
+      }
+    }
+
+    def successBlock[A](req: IdentifierRequest[A]) = Future.successful(Ok(JsString("success")))
+  }
+
+  private def checkErrorResult(actualResult: Result, expectedStatus: Int, expectedErrorCode: String, expectedErrorDescription: String) = {
+    actualResult.header.status shouldBe expectedStatus
+
+    val actualResultStream = actualResult.body.dataStream runWith StreamConverters.asInputStream()
+    val actualResultJson   = Json parse actualResultStream
+
+    actualResultJson \ "errorCode" shouldBe JsDefined(JsString(expectedErrorCode))
+    actualResultJson \ "errorDescription" shouldBe JsDefined(JsString(expectedErrorDescription))
+  }
+
+  private lazy val EXPECTED_400_ERROR_DESCRIPTION =
+    "Request data is invalid or missing. Please refer to API Documentation for further information"
+
+  private lazy val EXPECTED_500_ERROR_DESCRIPTION =
+    "The server encountered an error and couldn't process the request. Please refer to API Documentation for further information"
+}
