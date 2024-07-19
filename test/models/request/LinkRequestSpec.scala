b4f939297f75f74d1aebb9dbe738dbbd9abcf232
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-07-19 17:10:40 +0100

    TFCA-288 UT-ed invalid child DoB validation

--- test/models/request/LinkRequestSpec.scala
+++ test/models/request/LinkRequestSpec.scala
@@ -22,5 +22,22 @@ class LinkRequestSpec extends BaseSpec with models.request.Generators with Eithe
         errorKey shouldBe "/child_date_of_birth"
         errorValue shouldBe "error.path.missing"
       }
+
+    "report invalid child DoB" in
+      forAll(randomLinkRequestJsonWithInvalidChildDob) { json =>
+        val (jsPath, errors) =
+          json
+            .validate[LinkRequest]
+            .asEither
+            .left
+            .value
+            .loneElement
+
+        val errorKey   = jsPath.path.loneElement.toString
+        val errorValue = errors.loneElement.message
+
+        errorKey shouldBe "/child_date_of_birth"
+        errorValue shouldBe "error.expected.date.isoformat"
+      }
   }
 }
