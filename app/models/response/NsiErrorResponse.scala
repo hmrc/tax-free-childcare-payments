d285a96dff90ac4cf857da0574dfac9b2d2956df
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-08-05 17:19:51 +0100

    tfa-288 adjusted model to pass IT

--- app/models/response/NsiErrorResponse.scala
+++ app/models/response/NsiErrorResponse.scala
@@ -42,7 +42,7 @@ object NsiErrorResponse extends Enum[NsiErrorResponse] with Status {
   case object E0024 extends NsiErrorResponse(BAD_REQUEST, "Please check that the epp_reg_reference and epp_unique_customer_id are both correct")
   case object E0025 extends NsiErrorResponse(BAD_REQUEST, "Please check that the child_date_of_birth and outbound_child_payment_reference are both correct")
   case object E0026 extends NsiErrorResponse(BAD_REQUEST, "Please check the outbound_child_payment_ref supplied")
-  case object E0027 extends NsiErrorResponse(BAD_REQUEST, "Please check the outbound_child_payment_ref supplied")
+  case object E0027 extends NsiErrorResponse(BAD_REQUEST, "The CCP you have specified is not linked to the TFC Account. Please ensure that the parent goes into their TFC Portal and adds the CCP to their account first before attempting payment again later.")
 
   case object E0401 extends NsiErrorResponse(INTERNAL_SERVER_ERROR, "We encountered an error on our servers and did not process your request, please try again later.")
 
