1f67779b141fc8b14f899d099f864a4a1a5291a9
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-08-27 12:49:30 +0100

    TFCA-370 removed E0040 from error model

--- app/models/response/NsiErrorResponse.scala
+++ app/models/response/NsiErrorResponse.scala
@@ -56,7 +56,6 @@ object NsiErrorResponse extends Enum[NsiErrorResponse] with Status with Logging 
   case object E0035 extends NsiErrorResponse(BAD_REQUEST, "There is an issue with this TFC Account, please advise parent / carer to contact TFC customer Services")
   case object E0036 extends NsiErrorResponse(BAD_REQUEST, "Error processing payment due to Payee bank details")
 
-  case object E0040 extends NsiErrorResponse(BAD_REQUEST, "The outbound_child_payment_ref could not be found in the TFC system - please ensure parent checks their details and tries again.")
   case object E0041 extends NsiErrorResponse(BAD_REQUEST, "The epp_reg_reference could not be found in the TFC system. Please check the details and try again.")
   case object E0042 extends NsiErrorResponse(BAD_REQUEST, "The ccp_reg_reference could not be found in the TFC system or does not correlate with the ccp_postcode. Please check the details and try again.")
   case object E0043 extends NsiErrorResponse(BAD_REQUEST, "Parent associated with the bearer token does not have a TFC account. Please ask the parent to create a TFC account first.")
