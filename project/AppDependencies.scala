ce27ae6f47d90996bb1ad80ccafeddf3250c9321
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-09-06 15:03:59 +0100

    TFCA-393 updated Bootstrap and eliminated HttpClient V1

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion = "9.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"  %% "bootstrap-backend-play-30" % bootstrapVersion,
